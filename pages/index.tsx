fe978a5f4f605fa03d485c834be83d0cc9b9910f
PranavMaganti <vanpramaganti@gmail.com>
2021-08-15 12:14:48 +0100

    Add drop queries for clearing db to schema

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
     <div className="flex flex-col w-full items-center self-center mt-8 max-w-5xl">
       <h1 className="text-black text-5xl font-normal">
-        Welcocme to The Constellation Platform!
+        Welcome to The Constellation Platform!
       </h1>
       <p className="text-black text-2xl font-normal mt-4">
         Connecting local responses around the world
--- schema.sql
+++ schema.sql
@@ -1,4 +1,8 @@
-create type public.app_permission as enum ('users.delete', 'user_roles.insert', 'user_roles.delete', 'user_roles.update', 'topics.insert', 'topics.delete', 'topics.update');
+create type public.app_permission as enum (
+  'users.delete', 'user_roles.insert', 'user_roles.delete', 'user_roles.update', 
+  'topics.insert', 'topics.delete', 'topics.update', 
+  'posts.update', 'posts.delete', 'posts.insert'
+);
 create type public.app_role as enum ('admin', 'moderator', 'user');
 
 -- authorize with role-based access control (RBAC)
@@ -51,7 +55,34 @@ alter table public.topics enable row level security;
 create policy "Allow authorized insert access" on public.topics for insert with check ( authorize('topics.insert', auth.uid()) );
 create policy "Allow authorized update access" on public.topics for update using ( authorize('topics.update', auth.uid()) );
 create policy "Allow authorized delete access" on public.topics for delete using ( authorize('topics.delete', auth.uid()) );
-create policy "Allow all insert access" on public.topics for select using ( true );
+create policy "Allow all select access" on public.topics for select using ( true );
+
+
+-- Posts
+create table public.posts (
+  id text primary key,
+  user_id uuid references public.users not null,
+  topic_id text references public.topics not null,
+  title text not null,
+  description text,
+  content text not null,
+  created_at timestamp with time zone not null default now(),
+  salt_stage int,
+  views int not null default 0,
+  previous_salt_post_id int,
+  tags text[]
+); 
+create policy "Allow authorized insert access" on public.posts for insert with check ( 
+  auth.uid() = user_id or authorize('posts.insert', auth.uid()) 
+);
+create policy "Allow authorized update access" on public.posts for update using ( 
+  auth.uid() = user_id or authorize('posts.update', auth.uid()) 
+);
+create policy "Allow authorized delete access" on public.posts for delete using ( 
+  auth.uid() = user_id or authorize('posts.delete', auth.uid()) 
+);
+create policy "Allow all select access" on public.posts for select using ( true );
+
 
 
 -- USER ROLES
@@ -67,9 +98,6 @@ alter table public.user_roles enable row level security;
 create policy "Allow authorized insert access" on public.user_roles for insert with check ( authorize('user_roles.insert', auth.uid()) );
 create policy "Allow authorized update access" on public.user_roles for update using ( authorize('user_roles.update', auth.uid()) );
 create policy "Allow authorized delete access" on public.user_roles for delete using ( authorize('user_roles.delete', auth.uid()) );
-
-
-create table public.
 
 -- ROLE PERMISSIONS
 create table public.role_permissions (
@@ -96,12 +124,46 @@ create trigger on_auth_user_created
   after insert on auth.users
   for each row execute procedure public.handle_new_user();
 
-
-
-
 /**
  * REALTIME SUBSCRIPTIONS
  * Only allow realtime listening on public tables.
  */
 -- drop publication if exists supabase_realtime;
 -- create publication supabase_realtime for table products, prices;
+
+
+/**
+* Clear all tables, policies and triggers
+*/
+
+drop policy "Allow authorized insert access" on public.user_roles;
+drop policy "Allow authorized update access" on public.user_roles;
+drop policy "Allow authorized delete access" on public.user_roles;
+
+create policy "Allow all select access" on public.posts;
+create policy "Allow all select access" on public.posts;
+create policy "Allow authorized update access" on public.posts;
+create policy "Allow authorized insert access" on public.posts;
+
+drop policy "Allow authorized insert access" on public.topics;
+drop policy "Allow authorized update access" on public.topics;
+drop policy "Allow authorized delete access" on public.topics;
+drop policy "Allow all select access" on public.topics;
+
+drop policy "Allow logged-in read access" on public.users;
+drop policy "Allow individual update access" on public.users;
+drop policy "Allow authorized delete access" on public.users;
+
+drop trigger on_auth_user_created on auth.users;
+drop function public.authorize;
+drop function public.handle_new_user;
+
+drop table public.user_roles;
+drop table public.role_permissions;
+drop table public.topics;
+drop table public.users;
+drop table public.posts;
+
+drop type public.app_permission;
+drop type public.app_role;
+
