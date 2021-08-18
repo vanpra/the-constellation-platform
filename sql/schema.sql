aa3b7e7a7febbbea5a516c5befd13eec9a5d43f4
PranavMaganti <vanpramaganti@gmail.com>
2021-08-18 23:09:05 +0100

    Add supabase_realtime rule to schema file

--- sql/schema.sql
+++ sql/schema.sql
@@ -128,10 +128,5 @@ create trigger on_auth_user_created
  * REALTIME SUBSCRIPTIONS
  * Only allow realtime listening on public tables.
  */
--- drop publication if exists supabase_realtime;
--- create publication supabase_realtime for table products, prices;
-
-
-/**
-* Clear all tables, policies and triggers
-*/
+drop publication if exists supabase_realtime;
+create publication supabase_realtime for table posts;
