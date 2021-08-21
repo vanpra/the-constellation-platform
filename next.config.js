2eb5aeb89e5339555f324d2c0decf226c22ccf0c
PranavMaganti <vanpramaganti@gmail.com>
2021-08-22 00:42:49 +0100

    NextJS: Change config to use preact for prod build

--- next.config.js
+++ next.config.js
@@ -9,6 +9,22 @@ module.exports = {
       },
     ];
   },
+  webpack(config, { dev, isServer }) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+
+    return config;
+  },
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
