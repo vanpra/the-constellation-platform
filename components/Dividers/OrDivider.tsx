05d2b9efc11723b4d87f03239ae1ef60feed9b15
PranavMaganti <vanpramaganti@gmail.com>
2021-08-24 00:21:03 +0100

    Fix formatting and remove duplicate files

--- components/Dividers/OrDivider.tsx
+++ components/Dividers/OrDivider.tsx
@@ -6,12 +6,7 @@ interface OrDividerProps {
 
 export default function OrDivider({ className }: OrDividerProps) {
   return (
-    <div
-      className={classNames(
-        "flex items-center justify-center",
-        className
-      )}
-    >
+    <div className={classNames("flex items-center justify-center", className)}>
       <div className="flex-1 h-0.5 bg-black"></div>
       <p className="p-2 font-medium text-lg">OR</p>
       <div className="flex-1 h-0.5 bg-black"></div>
