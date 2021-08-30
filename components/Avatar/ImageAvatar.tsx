9cf8426556a0f178a98bdedfe658844379dac6ce
Michael Clasby <michaelclasby@hotmail.com>
2021-08-30 16:11:40 +0100

    Avatar: Remove surrounding div around img avatar

--- components/Avatar/ImageAvatar.tsx
+++ components/Avatar/ImageAvatar.tsx
@@ -9,21 +9,17 @@ export default function ImageAvatar(props: ImageAvatarProps) {
   const { className, avatarUrl, onClick } = props;
 
   return (
-    <div className={classNames("relative")}>
-      {
-        // eslint-disable-next-line @next/next/no-img-element
-        <img
-          src={avatarUrl}
-          alt="User Avatar"
-          className={classNames(
-            "flex items-center justify-center",
-            "border-2 border-black rounded-full",
-            { "hover:cursor-pointer": onClick != undefined },
-            className
-          )}
-          onClick={onClick}
-        />
-      }
-    </div>
+    // eslint-disable-next-line @next/next/no-img-element
+    <img
+      src={avatarUrl}
+      alt="User Avatar"
+      className={classNames(
+        "flex items-center justify-center",
+        "border-2 border-black rounded-full",
+        { "hover:cursor-pointer": onClick != undefined },
+        className
+      )}
+      onClick={onClick}
+    />
   );
 }
