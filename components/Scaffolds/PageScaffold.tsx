30fee6e63daf52de9e4b748f51f413e193260c2e
Michael Clasby <michaelclasby@hotmail.com>
2021-09-19 22:43:45 +0100

    Add button to page scafold

--- components/Scaffolds/PageScaffold.tsx
+++ components/Scaffolds/PageScaffold.tsx
@@ -4,12 +4,13 @@ import React from "react";
 interface PageScaffoldProps {
   title?: string;
   icon?: React.ReactNode;
+  button?: React.ReactNode;
   className?: string;
   children?: React.ReactNode;
 }
 
 export default function PageScaffold(props: PageScaffoldProps) {
-  const { title, icon, className, children } = props;
+  const { title, icon, button, className, children } = props;
   return (
     <div
       className={classNames(
@@ -18,12 +19,15 @@ export default function PageScaffold(props: PageScaffoldProps) {
         className
       )}
     >
-      <div className="flex flex-row items-center  mb-8">
+      <div className="flex flex-row items-center mb-8">
         {icon}
         {icon && <div className="w-3" />}
         {title && (
           <h1 className={classNames("font-medium text-5xl")}>{title}</h1>
         )}
+        <div className="ml-auto">
+          {button}
+        </div>
       </div>
 
       {children}
