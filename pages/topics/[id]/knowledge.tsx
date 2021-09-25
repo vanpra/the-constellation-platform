e59b558c106e5762f06bfb13522533cb666563fd
Michael Clasby <michaelclasby@hotmail.com>
2021-09-25 23:20:48 +0100

    Add knowledge page for topic

--- pages/topics/[id]/knowledge.tsx
+++ pages/topics/[id]/knowledge.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/dist/client/router";
 import React from "react";
 import PostCard from "../../../components/Cards/PostCard";
+import PostKnowledgeCard from "../../../components/Cards/PostKnowledgeCard";
 import ErrorDataLayout from "../../../components/Scaffolds/ErrorDataScaffold";
 import PageScaffold from "../../../components/Scaffolds/PageScaffold";
 import { usePostsByTopic } from "../../../utils/supabase/db";
@@ -9,21 +10,25 @@ export default function KnowledgeAssetPage() {
   const router = useRouter();
   const { id } = router.query;
   const { error, postsByTopic } = usePostsByTopic(id as string);
-
+  const postsWithKnowledge =
+    postsByTopic?.posts?.filter(
+      (post) => post.knowledge_asset && post.knowledge_asset.length > 0
+    ) || [];
   return (
     <>
-      <ErrorDataLayout error={error} data={postsByTopic}>
-        <PageScaffold title={postsByTopic?.topic + " - Knowledge Asset"} >
-          {postsByTopic?.posts.length === 0 && (
+      <ErrorDataLayout error={error} data={postsWithKnowledge}>
+        <PageScaffold title={postsByTopic?.topic + " - Knowledge Asset"}>
+          {postsWithKnowledge.length === 0 ? (
             <p className="text-2xl">
               There are currently no posts for this topic
             </p>
+          ) : (
+            <div className="flex flex-col space-y-6">
+              {postsWithKnowledge.map((post, index) => (
+                <PostKnowledgeCard key={index} post={post} />
+              ))}
+            </div>
           )}
-          <div className="flex flex-col space-y-6">
-            {postsByTopic?.posts?.map((post) => (
-              <PostCard key={post.id} post={post} />
-            ))}
-          </div>
         </PageScaffold>
       </ErrorDataLayout>
     </>
